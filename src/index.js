eb5f6e1f95d2dd3629d4ca16cbeb2a4f9c053840
misakiksw <80464994+misakika@users.noreply.github.com>
2022-08-14 11:08:17 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// let a = { bbb: "hey" };
+
+// console.log(a);
+
+// let { bbb } = a;
+
+// console.log(bbb);
+
+// bbb = "mmmmmm";
+
+// console.log(bbb);
+
+// console.log(a);
+
+// let object = {
+//   prop1: {
+//     prop2: 4
+//   }
+// };
+
+// console.log(object);
+
+// let { prop1 } = object
+
+// prop1.prop2=2
+
+// console.log(prop1)
+// console.log(object)
